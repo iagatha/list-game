655bb909afdfbc85644bcff80eac6646302ebe47
iagatha <iagathasouzaa@gmail.com>
2023-05-12 10:14:36 -0300

    game seed

--- src/main/java/com/myapp/dlist/entities/Game.java
+++ src/main/java/com/myapp/dlist/entities/Game.java
@@ -16,8 +16,13 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+
+
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
@@ -26,12 +31,13 @@ public class Game {
     }
 
     public Game(Long id, String title, Integer year, String genre,
-                String platforms, String imgUrl, String shortDescription, String longDescription) {
+                String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -75,6 +81,15 @@ public class Game {
 
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
+    }
+
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
